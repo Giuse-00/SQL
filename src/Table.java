ea99440cf8e50ebfd7363a8ea09ded6e1374412a
Giuseppe Iacono <giuseiacono10@gmail.com>
2023-04-06 21:23:14 +0200

    update: SELECT exercise done

--- src/Table.java
+++ src/Table.java
@@ -1,9 +1,12 @@
 import java.sql.*;
 import java.sql.DriverManager;
+import java.util.ArrayList;
 
 public class Table {
 
     public static void main(String[] args) {
+
+        ArrayList<String> arrayLastNames = new ArrayList<>();
 
         try{
 
@@ -14,8 +17,13 @@ public class Table {
 
             ResultSet resultSet = statement.executeQuery("SELECT * FROM students");
 
-            while (resultSet.next()){
-                System.out.println(resultSet.getString("first_name"));
+           while (resultSet.next()){
+                String firstNames = resultSet.getString("first_name");
+                String lastNames = resultSet.getString("last_name");
+
+               arrayLastNames.add(lastNames);
+
+               System.out.println(arrayLastNames);
             }
 
         } catch (SQLException e) {
